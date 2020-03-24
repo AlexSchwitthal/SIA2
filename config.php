47f62df31c4754bdbb9f05597633f816df27936d
Alexandre <Schwitthal.alexandre@gmail.com>
2020-03-24 16:29:56 +0100

    add gitignore

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_SERVER', 'localhost');	  // serveur MySql
-define('DB_DATABASE', 'EPA');	      // nom de la base de données
-define('DB_USER', 'root');		      // nom d'utilisateur
-define('DB_PWD', '');               // mot de passe
-
- ?>
