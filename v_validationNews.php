b8e73d0d73cb13fef9fa033e0e7c71181c9df2e1
leclercthomas <56258113+leclercthomas@users.noreply.github.com>
2020-03-27 17:07:02 +0100

    Delete v_validationNews.php

--- v_validationNews.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-info" role="alert">
-    <p>News publiée ! <a href="index.php?uc=gestionEtudiant">Cliquez ici</a> pour revenir à l'accueil.</p>
-</div>
-<?php /* header ("Refresh: 3;URL=index.php?uc=gestionEtudiant"); */ ?>
