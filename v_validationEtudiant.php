fd2988e3301e5ab0403e1dcb911599f4294bbea7
leclercthomas <56258113+leclercthomas@users.noreply.github.com>
2020-03-27 17:06:51 +0100

    Delete v_validationEtudiant.php

--- v_validationEtudiant.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-info" role="alert">
-    <p>Inscription réussi ! <a href="index.php?uc=gestionEtudiant">Cliquez ici</a> pour revenir à l'accueil.</p>
-</div>
-<?php /* header ("Refresh: 3;URL=index.php?uc=gestionEtudiant"); */ ?>
