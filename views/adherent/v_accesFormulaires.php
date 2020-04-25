526e21d664a1d1f7f797066f661426025c65c41e
SOFred <frederic_so27@hotmail.fr>
2020-04-25 17:14:45 +0200

    Ajout .png

--- views/adherent/v_accesFormulaires.php
+++ views/adherent/v_accesFormulaires.php
@@ -4,18 +4,18 @@
 <div class="col-md-10">
   <h2><b>Accès Formulaires</b></h2>
     <ul>
-        <li><a href="./img/form1">1 - Bulletin d'adhésion</a></li>
-        <li><a href="./img/form2">2 - Appel à cotisation</a></li>
-        <li><a href="./img/form3">3 - Modèle de reçu de dons</a></li>
-        <li><a href="./img/form4">4 - Convocation à l'assemblée générale</a></li>
-        <li><a href="./img/form5">5 - Pouvoir</a></li>
-        <li><a href="./img/form6">6 - Procès-verbal d'assemblée générale</a></li>
-        <li><a href="./img/form7">7 - Déclaration de modification des statuts</a></li>
-        <li><a href="./img/form8">8 - Fiche de remboursement de frais</a></li>
-        <li><a href="./img/form9">9 - Budget prévisionnel</a></li>
-        <li><a href="./img/form10">10 - Lettre de demande d'un devis d'assurance</a></li>
-        <li><a href="./img/form11">11 - Lettre de demande d'ouverture d'un compte bancaire</a></li>
-        <li><a href="./img/form12">12 - Demande de subvention</a></li>
-        <li><a href="./img/form13">13 - Demande de disposition d'un local municipal</a></li>
+        <li><a href="./img/form1.png">1 - Bulletin d'adhésion</a></li>
+        <li><a href="./img/form2.png">2 - Appel à cotisation</a></li>
+        <li><a href="./img/form3.png">3 - Modèle de reçu de dons</a></li>
+        <li><a href="./img/form4.png">4 - Convocation à l'assemblée générale</a></li>
+        <li><a href="./img/form5.png">5 - Pouvoir</a></li>
+        <li><a href="./img/form6.png">6 - Procès-verbal d'assemblée générale</a></li>
+        <li><a href="./img/form7.png">7 - Déclaration de modification des statuts</a></li>
+        <li><a href="./img/form8.png">8 - Fiche de remboursement de frais</a></li>
+        <li><a href="./img/form9.png">9 - Budget prévisionnel</a></li>
+        <li><a href="./img/form10.png">10 - Lettre de demande d'un devis d'assurance</a></li>
+        <li><a href="./img/form11.png">11 - Lettre de demande d'ouverture d'un compte bancaire</a></li>
+        <li><a href="./img/form12.png">12 - Demande de subvention</a></li>
+        <li><a href="./img/form13.png">13 - Demande de disposition d'un local municipal</a></li>
     </ul>
 </div>
