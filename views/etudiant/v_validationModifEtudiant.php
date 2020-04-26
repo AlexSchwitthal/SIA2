d393da12ee3ac6f8114d5ac489b8c647c197588e
leclercthomas <56258113+leclercthomas@users.noreply.github.com>
2020-04-26 13:24:22 +0200

    Ajout affichage validation modification

---
+++ views/etudiant/v_validationModifEtudiant.php
@@ -0,0 +1,8 @@
+<div class="col-md-10">
+  <div class="marge">
+    <div class="alert alert-info" role="alert">
+        <p>Modification effectuée ! <a href="index.php?uc=gestionEtudiant&action=affichageEtudiant">Cliquez ici</a> pour revenir à l'accueil.</p>
+    </div>
+  </div>
+</div>
+<?php /* header ("Refresh: 3;URL=index.php?uc=gestionEtudiant"); */ ?>
