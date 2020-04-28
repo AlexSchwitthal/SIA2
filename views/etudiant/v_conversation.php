f24927df446a1c0546fe1fe091ceabc57b52378d
alexSchwitthal <schwitthal.alexandre@gmail.com>
2020-04-28 18:03:21 +0200

    fix merge 4

--- views/etudiant/v_conversation.php
+++ views/etudiant/v_conversation.php
@@ -1,4 +1,4 @@
-<div class="col-md-10">
+<div class="col-md-9">
   <div class="marge">
   	<?php echo "<button type=\"button\" onclick=\"window.location='./index.php?uc=gestionEtudiant&action=messagerie';\">Retour à la messagerie</button> " ?>
     <table class="table table-bordered table-responsive table-striped">
@@ -16,19 +16,19 @@
           <td> <?php echo $message['texte'] ?></td>
           <td> <?php echo $message['date'] ?></td>
         </tr>
-        
+
         <?php
       }
       ?>
       <tr>
-        <td colspan="3"> 
+        <td colspan="3">
         	<form method="post" action="index.php?uc=gestionEtudiant&action=envoyerMessage">
-        		Nouveau message 
+        		Nouveau message
         		<input type="text" id="message" name="message" size="70">
         		<?php echo "<input type=\"hidden\" name=\"dest\"  value=\"{$dest}\">"?>
         		<?php echo "<input type=\"hidden\" name=\"expe\"  value=\"{$expe}\">"?>
-        		<input type="submit" value="Envoyer"> 
-        	</form> 
+        		<input type="submit" value="Envoyer">
+        	</form>
         </td>
         </tr>
     </table>
