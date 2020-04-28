8674759efca8c8970a903f2581ef670db9727d09
leclercthomas <56258113+leclercthomas@users.noreply.github.com>
2020-04-28 16:10:29 +0200

    Ajout messagerie 1

---
+++ views/etudiant/v_messagerie.php
@@ -0,0 +1,38 @@
+<div class="col-md-10">
+  <div class="marge">
+    <table class="table table-bordered table-responsive table-striped">
+      <tr>
+        <td> Contact </td>
+        <td> Dernier Message </td>
+        <td> Date </td>
+        <td> Action </td>
+      </tr>
+      <?php
+      foreach ($lesMessages as $message) {
+      	$expe=$pdo->getUserConnecte($_SESSION['logs']);
+      	if($expe['id']==$message['id_expediteur']){
+      		$FindContact=$message['id_destinataire'];
+      	}else{
+      		$FindContact=$message['id_expediteur'];
+      	}
+      	$contact = $pdo->getUserById($FindContact);
+        ?>
+        <tr>
+          <td> <?php echo $contact['username'] ?></td>
+          <td> <?php echo $message['texte'] ?></td>
+          <td> <?php echo $message['date'] ?></td>
+          <td>
+          	<?php
+          		$dest=$expediteur['id'];
+				$expe=$pdo->getUserConnecte($_SESSION['logs']);
+          		echo "<button type=\"button\" onclick=\"window.location='./index.php?uc=gestionEtudiant&action=conversation&id_destinataire={$contact['id']}&id_expediteur={$expe['id']}';\">Afficher la conversation</button> " 
+          	?>
+          	<button type="button" onclick="alert('You pressed the button!')">Effacer</button> 
+          </td>
+        </tr>
+        <?php
+      }
+      ?>
+    </table>
+  </div>
+</div>
