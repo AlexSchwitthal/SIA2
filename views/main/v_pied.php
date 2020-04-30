7bd5f336301bbf98232ea4feff1a3972881d6199
SOFred <frederic_so27@hotmail.fr>
2020-04-30 16:17:11 +0200

    footer

--- views/main/v_pied.php
+++ views/main/v_pied.php
@@ -4,5 +4,109 @@
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.jqueryui.min.js"></script>
     <script type="text/javascript" src="./js/pagination.js"></script>
     <script type="text/javascript" src="./js/script.js"></script>
+    <footer>
+  <div class="footer-gray">
+    <div class="footer-custom">
+      <div class="footer-lists">
+        <div class="footer-list-wrap">
+          <h6 class="ftr-hdr">Contact</h6>
+          <ul class="ftr-links-sub">
+            <li>XX-XX-XX-XX-XX</li>
+            <li>epa@gmail.com</li>
+          </ul>
+          <h6 class="ftr-hdr">International</h6>
+          <ul class="ftr-links-sub">
+            <li><a href="http://www.art.fr" rel="nofollow">France</a></li>
+            <li><a href="https://www.art.co.uk" rel="nofollow">Afrique</a></li>
+          </ul>
+        </div>
+        <!--/.footer-list-wrap-->
+
+        <div class="footer-list-wrap">
+          <h6 class="ftr-hdr">À propos d'EPA</h6>
+          <ul class="ftr-links-sub">
+            <li><a href="/asp/aboutus/default-asp/_/posters.htm" rel="nofollow">Notre association</a></li>
+            <li><a href="http://corporate.art.com/careers" rel="nofollow">Notre histoire</a></li>
+            <li><a href="/asp/landing/artistrising" rel="nofollow">Nos valeurs</a></li>
+            <li><a href="/~/art-for-business" rel="nofollow">Nos partenaires</a></li>
+          </ul>
+        </div>
+        <!--/.footer-list-wrap-->
+        <div class="footer-list-wrap">
+          <h6 class="ftr-hdr">Mon compte</h6>
+          <ul class="ftr-links-sub">
+            <art:content rule="!loggedin">
+              <li class="ftr-Login"><span class="link login-trigger">Accéder à mon compte</span></li>
+              <li><span class="link" onclick="link('/asp/secure/your_account/track_orders-asp/_/posters.htm')">Sécurité et connexion</span></li>
+              <li class="ftr-Login"><span class="link login-trigger">Notifications</span></li>
+            </art:content>
+          </ul>
+        </div>
+        <!--/.footer-list-wrap-->
+      </div>
+      <!--/.footer-lists-->
+      <div class="footer-email">
+        <h6 class="ftr-hdr">Newsletter</h6>
+        <div id="ftr-email" class="ftr-email-form">
+          <form id="ftrEmailForm" method="post" action="http://em.art.com/pub/rf">
+            <div class="error">Entrez une adresse mail valide</div>
+            <input type="text" name="email_address_" id="ftrEmailInput" class="input" placeholder="Entrez une adresse mail valide" />
+            <!--
+                -->
+            <input type="submit" class="button" value="Je m'abonne" style="width:150px" />
+            <input type="hidden" name="country_iso2" value="">
+            <input type="hidden" name="language_iso2" value="en">
+            <input type="hidden" name="site_domain" value="art.com">
+            <input type="hidden" name="email_acq_source" value="01-000001">
+            <input type="hidden" name="email_acq_date" value="" id="ftr-email-date">
+            <input type="hidden" name="brand_id" value="ART">
+            <input type="hidden" name="_ri_" value="X0Gzc2X%3DWQpglLjHJlYQGnp51yrMf2qXdC9tjU8pzgMtwfYzaVwjpnpgHlpgneHmgJoXX0Gzc2X%3DWQpglLjHJlYQGnyLSq2fzdkuzdzglHMsUhgeNzaSgkk">
+          </form>
+        </div>
+        <!--/.ftr-email-form-->
+        <div class="ftr-email-privacy-policy"></div>
+      </div>
+      <!--/.footer-email-->
+      <div class="footer-social">
+        <h6 class="ftr-hdr">Suivez-nous</h6>
+        <ul>
+          <li>
+            <a href="https://www.facebook.com/" title="Facebook" onclick="_gaq.push(['_trackSocial', 'Facebook', 'Follow', 'Footer', 'undefined', 'True']);">
+              <img width="24" height="24" alt="Like us on Facebook" src="http://cache1.artprintimages.com/images/jump_pages/rebrand/footer/fb.png">
+            </a>
+          </li>
+          <li>
+            <a href="https://google.com" title="Google+" onclick="_gaq.push(['_trackSocial', 'GooglePlus', 'Follow', 'Footer', 'undefined', 'True']);">
+              <img width="24" height="24" alt="Follow us on Google+" src="http://cache1.artprintimages.com/images/jump_pages/rebrand/footer/gplus.png">
+            </a>
+          </li>
+          <li>
+            <a href="https://pinterest.com/" target="_blank">
+              <img width="24" height="24" alt="Follow us on Pinterest" src="http://cache1.artprintimages.com/images/jump_pages/rebrand/footer/pin-badge.png">
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="http://instagram.com/">
+              <img width="24" height="24" alt="Follow us on Instagram" src="http://cache1.artprintimages.com/images/jump_pages/rebrand/footer/instagram-badge.png">
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/" title="Twitter" onclick="_gaq.push(['_trackSocial', 'Twitter', 'Follow', 'Footer', 'undefined', 'True']);">
+              <img width="67" alt="Follow us on Twitter" src="http://cache1.artprintimages.com/images/jump_pages/rebrand/footer/twitter.png">
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!--/.footer-social-->
+      <div class="footer-legal">
+        <p>&copy; EPA SIA2 2020. | <a href="/help/privacy-policy.html" rel="nofollow">Politique de confidentialité</a> | <a href="/help/terms-of-use.html" rel="nofollow">Condition d'utilisations</a> | <a href="/help/terms-of-sale.html" rel="nofollow">Conditions de ventes</a></p>
+      </div>
+      <!--/.footer-legal-->
+    </div>
+    <!--/.footer-custom-->
+  </div>
+  <!--/.footer-gray-->
+</footer>
   </body>
+
 </html>
