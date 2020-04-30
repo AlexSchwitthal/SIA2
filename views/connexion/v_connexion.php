9b4044f70b0bb6b084fc8a6812194e5e03c4fbcf
000GH000 <55983249+000GH000@users.noreply.github.com>
2020-04-30 06:13:31 -0400

    Mofication front-end page connexion.php

--- views/connexion/v_connexion.php
+++ views/connexion/v_connexion.php
@@ -1,19 +1,25 @@
-<table>
-  <form name="form" method="post" action="index.php?uc=connexion&action=valideConnexion">
-    <tr>
-      <td> identifiant : </td>
-      <td><input type="text" id="login" name="login"> </td>
-    </tr>
+<div id="contentPage">
+    <h2 class="pageTitle">Se connecter</h2>
+    <div style="width:33%; margin:auto;">
+        <form id="primoArrivant" name="form" method="post" action="index.php?uc=connexion&action=valideConnexion">
+            <div class="formDiv">
+                <table>
+                    <col style="width:35%">
+                    <col style="width:65%">
+                    
+                    <tr>
+                        <td>Email :</td>
+                        <td><input type="text" id="login" name="login"> </td>
+                    </tr>
 
-    <tr>
-      <td> mot de passe : </td>
-      <td><input type="password" id="mdp" name="mdp"> </td>
-    </tr>
-
-    <tr>
-      <td colspan="2">
-        <input type="submit">
-      </td>
-    </tr>
- </form>
-</table>
+                    <tr>
+                        <td>Mot de passe :</td>
+                        <td><input type="password" id="mdp" name="mdp"> </td>
+                    </tr>
+                
+                </table>
+                <input type="submit" value="Se connecter" style="float:right">
+            </div>
+        </form>
+    </div>
+</div>
