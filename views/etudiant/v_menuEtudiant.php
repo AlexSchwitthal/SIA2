9934f89efa8348654b3ba3f8e4989fde1668aa64
000GH000 <55983249+000GH000@users.noreply.github.com>
2020-04-30 05:10:59 -0400

    Mofications du menu vertical section etudiant

--- views/etudiant/v_menuEtudiant.php
+++ views/etudiant/v_menuEtudiant.php
@@ -1,74 +1,26 @@
 		<div id="menuVertical" class="col-md-3">
-            <script language="javascript" type="text/javascript" src="views/etudiant/etudiant.js"></script>
+    
                 <ul id="menuList">
                     <?php if(!isset($_SESSION['groupe'])) { ?>
-                        <li <?php if (!isset($_REQUEST['uc']) || $_REQUEST['uc'] == 'accueil') { ?> class="active"<?php } ?>>
-                            <div id="menu1" onclick="showSubMenu('menu1')"><a href="#"><p>Accueil</p></a></div>
-                            <ul class="subMenu">
-                                <li><a href="#">Link 1</a></li>
-                                <li><a href="#">Link 2</a></li>
-                                <li><a href="#">Link 3</a></li>
-                            </ul>
-                        </li>
-
-                        <li <?php if (isset($_REQUEST['uc']) && $_REQUEST['uc'] == 'gestionAdherent') { ?> class="active"<?php } ?>>
-                            <div id="menu2" onclick="showSubMenu('menu2')"><a href="#"><p>Devenir adhérent</p></a></div>
-                            <ul class="subMenu">
-                                <li><a href="#">Link 1</a></li>
-                                <li><a href="#">Link 2</a></li>
-                                <li><a href="#">Link 3</a></li>
-                            </ul>
-                        </li>
 
                         <li <?php if (isset($_REQUEST['uc']) && $_REQUEST['uc'] == 'gestionEtudiant') { ?> class="active"<?php } ?>>
-                            <div id="menu3" onclick="showSubMenu('menu3')"><a href="#"><p>Accueil étudiant</p></a></div>
+                            <div id="menu3"><a href="#"><p>Accueil étudiant</p></a></div>
                             <ul class="subMenu">
                                 <li><a href="index.php?uc=gestionEtudiant&action=inscriptionEtudiant">Formulaire Primo Arrivant</a></li>
-                                <li><a href="#">Informations Vie Étudiante</a></li>
-                                <li><a href="#">Link 3</a></li>
+                                <li><a href="index.php">Informations Vie Étudiante</a></li>
+                                <li><a href="#">FAQ</a></li>
                             </ul>
                         </li>
-
-                        <li <?php if (isset($_REQUEST['uc']) && $_REQUEST['uc'] == 'don') { ?> class="active"<?php } ?>>
-                            <div id="menu4" onclick="showSubMenu('menu4')"><a href="#"><p>Faire un don</p></a></div>
-                            <ul class="subMenu">
-                                <li><a href="#">Link 1</a></li>
-                                <li><a href="#">Link 2</a></li>
-                                <li><a href="#">Link 3</a></li>
-                            </ul>
-                        </li>
-
-                        <li <?php if (isset($_REQUEST['uc']) && $_REQUEST['uc'] == 'forum') { ?> class="active"<?php } ?>>
-                            <div id="menu5" onclick="showSubMenu('menu5')"><a href="#"><p>Forum</p></a></div>
-                            <ul class="subMenu">
-                                <li><a href="#">Link 1</a></li>
-                                <li><a href="#">Link 2</a></li>
-                                <li><a href="#">Link 3</a></li>
-                            </ul>
-                        </li>
-
-                        <li <?php if (isset($_REQUEST['uc']) && $_REQUEST['uc'] == 'connexion') { ?> class="active"<?php } ?>>
-                            <div id="menu6" onclick="showSubMenu('menu6')"><a href="#"><p>Connexion</p></a></div>
-                            <ul class="subMenu">
-                                <li><a href="#">Link 1</a></li>
-                                <li><a href="#">Link 2</a></li>
-                                <li><a href="#">Link 3</a></li>
-                            </ul>
-                        </li>
+                        
                     <?php } ?>
 
                     <?php if(userGroupe(3) || userGroupe(2) || userGroupe(1) ){ ?>
                         <li>
-                            <div><a href="#">Accueil</a></div>
-                        </li>
-                        <li>
-                            <div><a href="#">Voir étudiants</a></div>
-                        </li>
-                        <li>
-                            <div><a href="#">Faire un don</a></div>
-                        </li>
-                        <li>
-                            <div><a href="#">Forum</a></div>
+                            <div id="menu3"><a href="#"><p>Espace étudiant</p></a></div>
+                            <ul class="subMenu">
+                                <li><a href="index.php?uc=gestionEtudiant&action=modifierEtudiant">Mon compte</a></li>
+                                <li><a href="index.php?uc=gestionEtudiant&action=messagerie">Messagerie</a></li>
+                            </ul>
                         </li>
                     <?php } ?>
 
@@ -78,7 +30,4 @@
                     </li>
                     <?php } ?>
                 </ul>
-                <script>
-                    hideAllSubMenus();
-                </script>
 		</div>
