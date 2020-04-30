80073d2ec3610feee8d264c1e7a19501d177a698
000GH000 <55983249+000GH000@users.noreply.github.com>
2020-04-30 05:12:14 -0400

    Mofication front-end page pageNews.php

--- views/news/v_pageNews.php
+++ views/news/v_pageNews.php
@@ -1,14 +1,15 @@
-<div class="col-md-12">
-    <h2 class="pageTitle"><?php echo $laNews['titre'] ?></h2>
-    <div>
-      <?php echo $laNews['description'] ?>
+<div id="contentPage" class="col-md-12">
+    <div style="margin-top: 20px;margin-left: 30px">
+        <a href="index.php?uc=news"> Retour à la liste des news </a>
     </div>
-    <br>
-    <div>
-      publié le <?php echo conversionDate($laNews['datePublication']); ?>
+    <div style="width:70%; margin-left:auto; margin-right: auto; margin-top: 20px;background-color:green; padding-top: 15px; padding-bottom: 30px; padding-left: 25px;padding-right:25px; text-align:justify; background-color: #f9f4db; min-height: 600px;">
+        <h2 class="pageTitle"><?php echo $laNews['titre'] ?></h2>
+        <div>
+            publié le <?php echo conversionDate($laNews['datePublication']); ?>
+        </div>
+        </br>
+        <div>
+            <?php echo $laNews['description'] ?>
+        </div>
     </div>
-
-    <br>
-    <div>
-      <a href="index.php?uc=news"> revenir à la liste des news </a>
 </div>
